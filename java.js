19c7b0929422270d7cf403293c6e7939a980bd48
Joao22041992 <joaomateus89@gmail.com>
2025-10-23 13:02:27 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,21 @@
+// Script de validação do Bootstrap
+(() => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
